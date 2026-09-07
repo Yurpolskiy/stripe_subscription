90dee1c542536332bc4451c6d85664f7f660960b
Yurpolskyi <fpbidni@gmail.com>
2026-09-07 16:31:20 +0200

    Initialized project

---
+++ backend/src/adapters/http/server.ts
@@ -0,0 +1,22 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+    logger: true
+})
+
+fastify.get('/', (request, reply) => {
+    reply.send({hello: 'world'})
+})
+
+
+const start = async() => {
+    try {
+        await fastify.listen({port: 3000})
+        console.log("Server listening on port 3000")
+    } catch(err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
